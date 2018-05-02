de1c29e985f1a51383593f50285ba79de6c34a69
Michael O%27Brien <mobrien@colgate.edu>
2018-05-02 21:03:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,11 +2,11 @@ function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 0) {
       array.push()
-      console.log(I am 1 strange loop.)
+      console.log("I am 1 strange loop.")
     }
     else {
       array.push() 
-      console.log(I am ${i} strange loops.)
+      console.log("I am ${i} strange loops.")
     }
   }
 }
