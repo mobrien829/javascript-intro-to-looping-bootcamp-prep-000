d1f0e4cec0b9719011d79db4c59f9ddc6b841181
Michael O%27Brien <mobrien@colgate.edu>
2018-05-02 20:56:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,8 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      array
-    else if (i > 1) {
-      
+      console.log( "I am 1 strange loop.")
     }
-  }
-}
+    else if (i > 1) {
+      array.push(console.log ("I am ${i} strange loops."))
+    }
