bae1ed157e09631bbf02771afe08bdd5bc878432
Michael O%27Brien <mobrien@colgate.edu>
2018-05-02 21:02:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,7 +4,8 @@ function forLoop(array) {
       array.push()
       console.log("I am 1 strange loop.")
     }
-    else array.push() {
+    else {
+      array.push() 
       console.log("I am ${i} strange loops.")
     }
   }
